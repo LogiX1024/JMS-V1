ad04909547bfbb2fb8d79028e77cde9c8b9c49a3
Thejan Rajapakshe <coder.clix@gmail.com>
2015-11-17 19:53:26 +0530

    Email Sender

---
+++ application/views/email/invite_reviewer.php
@@ -0,0 +1,19 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+    "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+    <title>eJournal</title>
+</head>
+<body>
+<h3>Invitation to eJournal</h3>
+
+<p>Hello {name},</p>
+
+<p>You are invited to register to the eJournal Online system of Faculty of Applied Sciences, Rajarata University of Sri
+    Lanka.</p>
+
+<p>Please follow the <a href="{link}">link</a> to Register. </p>
+<br/>
+<p style="text-align: center">Thank You</p>
+</body>
+</html>
