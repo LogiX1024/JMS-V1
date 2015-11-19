f8f84e4abd6fc41826f49510f19e02db9ce92f89
Thejan Rajapakshe <coder.clix@gmail.com>
2015-11-19 20:06:07 +0530

    Finished Author Submission _ Dashboard

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,8 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost:8088';
+$config['base_url']	= 'http://appjms.azurewebsites.net/';
+//$config['base_url']	= 'http://localhost:8088';
 
 /*
 |--------------------------------------------------------------------------
