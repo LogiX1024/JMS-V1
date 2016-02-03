d416a02b6070ea996a133ea49fc4286b8033707b
Milinda <Milinda@DESKTOP-9B029VG>
2016-02-03 21:03:06 +0530

    author view reviews finished. reviewer_review file upload need.

--- application/models/review.php
+++ application/models/review.php
@@ -10,7 +10,7 @@ class Review extends CI_Model {
         $this->db->select('id,review_date,reviewer_id,title_acceptable,title_suggession,
             introduction,introduction_suggession,originality,clarity,completeness,interpretation,
             importance,materials_and_methods,materials_and_methods_suggession,
-            results_and_discussion,results_and_discussion_suggession,decision');
+            results_and_discussion,results_and_discussion_suggession,decision,fileurl');
         $this->db->from('review');
         $this->db->where('id', $id);
         $result = $this->db->get()->result();        
