0b99e2ab6682cfde8ad4ac031980e57bedbc9cce
Milinda <Milinda@DESKTOP-9B029VG>
2016-02-12 19:35:41 +0530

    view feedback UI done

--- application/views/editor_view_feedback.php
+++ application/views/editor_view_feedback.php
@@ -19,13 +19,13 @@ and open the template in the editor.
 
             <div class="row wrapper border-bottom white-bg page-heading">
                 <div class="col-sm-4">
-                    <h2><span class="fa fa-user"></span> Reviewing</h2>
+                    <h2><span class="fa fa-user"></span> Feedbacks</h2>
                     <ol class="breadcrumb">
                         <li>
-                            Reviewer
+                            Editor
                         </li>
                         <li class="active">
-                            <strong>Reviewing</strong>
+                            <strong>Feedbacks</strong>
                         </li>
                     </ol>
                 </div>
@@ -44,55 +44,39 @@ and open the template in the editor.
                         </center>
                     </div>
 
+                    <?php
+                        for($x=0;$x<3;$x++){
+                    ?>
+                        <div class="media well">
+                            <div class="media-body">
+                                <div class="col-lg-10">
+                                    <div class="ibox-content">
+                                        <h3 class="media-heading">
+                                            <span>Title : </span>
+                                        </h3>
+                                        
+                                        <span>Author : </span><br/>
+                                        <span>Journal :  </span><br/>
+                                        <span>Submitted Date :  </span><br/>
+                                    </div>
+                                </div>
 
-                    <div class="form-group">
-                        <div class="col-sm-2"><h4>Select Journal</h4></div>
+                                <div class="col-lg-2 pull-right">
+                                   
+                                    <a href="" target="_blank">
+                                        <button type="button" style="margin-bottom: 10px"
+                                                class="btn btn-w-m btn-default pull-right">View Feedback
+                                        </button>
+                                    </a>
+                                </div>
 
-                        <div class="col-sm-4">
-                            <select class="form-control m-b" id="journal" name="journal">
-                                <option value="1">Journal 1</option>
-                                <option value="1">Journal 2</option>
-                                <option value="1">Journal 3</option>
-                                <option value="1">Journal 4</option>
-                            </select>
+                            </div>
                         </div>
-                    </div>
-                    
-                    
-                    <table class="table table-hover">
-                        <thead>
-                            <tr>
-                                <th>Article ID</th>
-                                <th>Title</th>
-                                <th>Author</th>
-                                <th>Feedback</th>
-                                <th>Proofread</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>1</td>
-                                <td><span class="pie">0.52,1.041</span></td>
-                                <td>Samantha</td>
-                                <td class="text-navy"> <i class="fa fa-level-up"></i> 40% </td>
-                                <td>Samantha</td>
-                            </tr>
-                            <tr>
-                                <td>2</td>
-                                <td><span class="pie">226,134</span></td>
-                                <td>Jacob</td>
-                                <td class="text-warning"> <i class="fa fa-level-down"></i> -20% </td>
-                                <td>Samantha</td>
-                            </tr>
-                            <tr>
-                                <td>3</td>
-                                <td><span class="pie">0.52/1.561</span></td>
-                                <td>Damien</td>
-                                <td class="text-navy"> <i class="fa fa-level-up"></i> 26% </td>
-                                <td>Samantha</td>
-                            </tr>
-                        </tbody>
-                    </table>
+                    <?php
+                        }
+                    ?>
+
+
 
 
                 </div>
